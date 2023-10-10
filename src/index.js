c5509d875f923e18c1581c965472e4a8c7e45d85
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2023-10-10 10:06:04 +0200

    Resolve merge conflict

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "tw-elements-react/dist/css/tw-elements-react.min.css";
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import CurrentWeather from "./components/Weather/CurrentWeather";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <CurrentWeather />
   </React.StrictMode>
 );
 
