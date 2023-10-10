3b6c202f1b804e3de6b04e716ed774e5bd78afca
N3tscape <sofiane.debrabant.pro@gmail.com>
2023-10-10 14:36:58 +0200

    Refactoring useWeatherData

--- src/hooks/useWeatherData.js
+++ src/hooks/useWeatherData.js
@@ -1,16 +1,5 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
-
-const API_URL = 'https://api.open-meteo.com/v1/forecast?latitude=50.6927049&longitude=3.177847&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&daily=sunrise,sunset&daily=precipitation_sum&timezone=auto';
-
-const fetchWeatherData = async () => {
-    try {
-        const response = await axios.get(API_URL);
-        return response.data;
-    } catch (error) {
-        return Promise.reject(error);
-    }
-}
+import fetchWeatherData from '../services/fetchWeatherData';
 
 function useWeatherData() {
     const [weatherData, setWeatherData] = useState(null);
